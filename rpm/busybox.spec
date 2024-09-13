sailfishos/busybox

commit be6020a4aca08f1e30d73f7cf1ac20342adca1f4

[busybox] Update to 1.36.1. Fixes JB#59920
Disable some new config options.

rpm/0004-ash-Load-ENV-file-also-if-SSH_CLIENT-SSH2_CLIENT-is-.patch
@@ -11,22 +11,18 @@ This emulates the behavior of bash loading .bashrc when $SSH_CLIENT or $SSH2_CLI
 
 Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>
 ---
- shell/ash.c | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
+ shell/ash.c | 1 +
+ 1 file changed, 1 insertion(+)
 
 diff --git a/shell/ash.c b/shell/ash.c
-index a33ab0626..b336056e1 100644
+index 9344e4de1729c464af04d70754b2511af7af0cbe..cb2d4d4c3112ba5e8c4f846a217a21fce49d8b7c 100644
 --- a/shell/ash.c
 +++ b/shell/ash.c
-@@ -14471,7 +14471,7 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
+@@ -14726,6 +14726,7 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
  #ifndef linux
- 	 getuid() == geteuid() && getgid() == getegid() &&
+ 	 && getuid() == geteuid() && getgid() == getegid()
  #endif
--	 iflag
-+	 iflag || ( minusc && ( lookupvar("SSH_CLIENT") != NULL || lookupvar("SSH2_CLIENT") != NULL)) && rootshell
++	 || ( minusc && ( lookupvar("SSH_CLIENT") != NULL || lookupvar("SSH2_CLIENT") != NULL)) && rootshell
  	) {
  		const char *shinit = lookupvar("ENV");
  		if (shinit != NULL && *shinit != '\0')
--- 
-2.31.1
-

rpm/0005-ash-job-option-to-restore-term-io-after-job-is-stopp.patch
@@ -17,7 +17,7 @@ Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>
  1 file changed, 9 insertions(+), 1 deletion(-)
 
 diff --git a/shell/ash.c b/shell/ash.c
-index b336056e1..dcaec69c5 100644
+index cb2d4d4c3112ba5e8c4f846a217a21fce49d8b7c..9f07632f6ce16b8b3d144470f1c37bc4b0035779 100644
 --- a/shell/ash.c
 +++ b/shell/ash.c
 @@ -77,6 +77,14 @@
@@ -35,7 +35,7 @@ index b336056e1..dcaec69c5 100644
  //config:config ASH_ALIAS
  //config:	bool "Alias support"
  //config:	default y
-@@ -3796,7 +3804,7 @@ static unsigned njobs; //4
+@@ -3825,7 +3833,7 @@ static unsigned njobs; //4
  /* current job */
  static struct job *curjob; //lots
  
@@ -44,6 +44,3 @@ index b336056e1..dcaec69c5 100644
  /* Bash has a feature: it restores termios after a successful wait for
   * a foreground job which had at least one stopped or sigkilled member.
   * The probable rationale is that SIGSTOP and SIGKILL can preclude task from
--- 
-2.31.1
-

rpm/0006-ash-Write-history-on-SIGHUP.patch
@@ -16,13 +16,13 @@ Signed-off-by: Tomi Leppänen <tomi.leppanen@jolla.com>
  1 file changed, 20 insertions(+)
 
 diff --git a/shell/ash.c b/shell/ash.c
-index dcaec69c5..019182f57 100644
+index 9f07632f6ce16b8b3d144470f1c37bc4b0035779..79c30d4c9dad76a7963d89742e6a496891b398cf 100644
 --- a/shell/ash.c
 +++ b/shell/ash.c
-@@ -3666,6 +3666,19 @@ signal_handler(int signo)
+@@ -3692,6 +3692,19 @@ signal_handler(int signo)
+ 		if (!trap[SIGCHLD])
  			return;
  	}
- 
 +#if ENABLE_FEATURE_EDITING_SAVE_ON_EXIT
 +	if (signo == SIGHUP) {
 +		/*
@@ -36,10 +36,10 @@ index dcaec69c5..019182f57 100644
 +	}
 +#endif
 +
- 	gotsig[signo - 1] = 1;
- 	pending_sig = signo;
- 
-@@ -3738,6 +3751,12 @@ setsignal(int signo)
+ #if ENABLE_FEATURE_EDITING
+ 	bb_got_signal = signo; /* for read_line_input: "we got a signal" */
+ #endif
+@@ -3767,6 +3780,12 @@ setsignal(int signo)
  	if (signo == SIGCHLD)
  		new_act = S_CATCH;
  
@@ -52,7 +52,7 @@ index dcaec69c5..019182f57 100644
  	t = &sigmode[signo - 1];
  	cur_act = *t;
  	if (cur_act == 0) {
-@@ -9588,6 +9607,7 @@ setinteractive(int on)
+@@ -9762,6 +9781,7 @@ setinteractive(int on)
  	setsignal(SIGINT);
  	setsignal(SIGQUIT);
  	setsignal(SIGTERM);
@@ -60,6 +60,3 @@ index dcaec69c5..019182f57 100644
  	if (is_interactive > 1) {
  #if !ENABLE_FEATURE_SH_EXTRA_QUIET
  		/* Looks like they want an interactive shell */
--- 
-2.31.1
-

rpm/busybox-sailfish.config
@@ -1,7 +1,7 @@
 #
 # Automatically generated make config: don't edit
-# Busybox version: 1.34.1
-# Fri Jan  7 13:32:37 2022
+# Busybox version: 1.36.1
+# Wed May 31 10:01:57 2023
 #
 CONFIG_HAVE_DOT_CONFIG=y
 
@@ -93,10 +93,16 @@ CONFIG_FEATURE_BUFFERS_USE_MALLOC=y
 # CONFIG_FEATURE_BUFFERS_GO_IN_BSS is not set
 CONFIG_PASSWORD_MINLEN=6
 CONFIG_MD5_SMALL=1
+CONFIG_SHA1_SMALL=3
+CONFIG_SHA1_HWACCEL=y
+CONFIG_SHA256_HWACCEL=y
 CONFIG_SHA3_SMALL=1
-CONFIG_FEATURE_FAST_TOP=y
-# CONFIG_FEATURE_ETC_NETWORKS is not set
-# CONFIG_FEATURE_ETC_SERVICES is not set
+# CONFIG_FEATURE_NON_POSIX_CP is not set
+CONFIG_FEATURE_VERBOSE_CP_MESSAGE=y
+CONFIG_FEATURE_USE_SENDFILE=y
+CONFIG_FEATURE_COPYBUF_KB=4
+# CONFIG_MONOTONIC_SYSCALL is not set
+# CONFIG_IOCTL_HEX2STR_ERROR is not set
 CONFIG_FEATURE_EDITING=y
 CONFIG_FEATURE_EDITING_MAX_LEN=1024
 # CONFIG_FEATURE_EDITING_VI is not set
@@ -120,14 +126,9 @@ CONFIG_UNICODE_WIDE_WCHARS=y
 # CONFIG_UNICODE_BIDI_SUPPORT is not set
 # CONFIG_UNICODE_NEUTRAL_TABLE is not set
 # CONFIG_UNICODE_PRESERVE_BROKEN is not set
-# CONFIG_FEATURE_NON_POSIX_CP is not set
-CONFIG_FEATURE_VERBOSE_CP_MESSAGE=y
-CONFIG_FEATURE_USE_SENDFILE=y
-CONFIG_FEATURE_COPYBUF_KB=4
-# CONFIG_FEATURE_SKIP_ROOTFS is not set
-# CONFIG_MONOTONIC_SYSCALL is not set
-# CONFIG_IOCTL_HEX2STR_ERROR is not set
-# CONFIG_FEATURE_HWIB is not set
+# CONFIG_LOOP_CONFIGURE is not set
+# CONFIG_NO_LOOP_CONFIGURE is not set
+CONFIG_TRY_LOOP_CONFIGURE=y
 CONFIG_XATTR=y
 
 #
@@ -163,6 +164,8 @@ CONFIG_BZIP2_SMALL=0
 CONFIG_CPIO=y
 CONFIG_FEATURE_CPIO_O=y
 CONFIG_FEATURE_CPIO_P=y
+CONFIG_FEATURE_CPIO_IGNORE_DEVNO=y
+CONFIG_FEATURE_CPIO_RENUMBER_INODES=y
 # CONFIG_DPKG is not set
 # CONFIG_DPKG_DEB is not set
 CONFIG_GZIP=y
@@ -198,6 +201,22 @@ CONFIG_FEATURE_TAR_SELINUX=y
 #
 # Coreutils
 #
+CONFIG_FEATURE_VERBOSE=y
+
+#
+# Common options for date and touch
+#
+CONFIG_FEATURE_TIMEZONE=y
+
+#
+# Common options for cp and mv
+#
+CONFIG_FEATURE_PRESERVE_HARDLINKS=y
+
+#
+# Common options for df, du, ls
+#
+CONFIG_FEATURE_HUMAN_READABLE=y
 CONFIG_BASENAME=y
 CONFIG_CAT=y
 CONFIG_FEATURE_CATN=y
@@ -226,6 +245,7 @@ CONFIG_FEATURE_DD_IBS_OBS=y
 CONFIG_FEATURE_DD_STATUS=y
 CONFIG_DF=y
 CONFIG_FEATURE_DF_FANCY=y
+# CONFIG_FEATURE_SKIP_ROOTFS is not set
 CONFIG_DIRNAME=y
 # CONFIG_DOS2UNIX is not set
 # CONFIG_UNIX2DOS is not set
@@ -325,6 +345,7 @@ CONFIG_FEATURE_TR_CLASSES=y
 CONFIG_FEATURE_TR_EQUIV=y
 CONFIG_TRUE=y
 # CONFIG_TRUNCATE is not set
+# CONFIG_TSORT is not set
 CONFIG_TTY=y
 CONFIG_UNAME=y
 CONFIG_UNAME_OSNAME="GNU/Linux"
@@ -343,21 +364,6 @@ CONFIG_W=y
 CONFIG_USERS=y
 CONFIG_WHOAMI=y
 CONFIG_YES=y
-
-#
-# Common options
-#
-CONFIG_FEATURE_VERBOSE=y
-
-#
-# Common options for cp and mv
-#
-CONFIG_FEATURE_PRESERVE_HARDLINKS=y
-
-#
-# Common options for df, du, ls
-#
-CONFIG_FEATURE_HUMAN_READABLE=y
 
 #
 # Console Utilities
@@ -445,7 +451,11 @@ CONFIG_FEATURE_ALLOW_EXEC=y
 CONFIG_FIND=y
 CONFIG_FEATURE_FIND_PRINT0=y
 CONFIG_FEATURE_FIND_MTIME=y
+CONFIG_FEATURE_FIND_ATIME=y
+CONFIG_FEATURE_FIND_CTIME=y
 CONFIG_FEATURE_FIND_MMIN=y
+CONFIG_FEATURE_FIND_AMIN=y
+CONFIG_FEATURE_FIND_CMIN=y
 CONFIG_FEATURE_FIND_PERM=y
 CONFIG_FEATURE_FIND_TYPE=y
 CONFIG_FEATURE_FIND_EXECUTABLE=y
@@ -453,6 +463,7 @@ CONFIG_FEATURE_FIND_XDEV=y
 CONFIG_FEATURE_FIND_MAXDEPTH=y
 CONFIG_FEATURE_FIND_NEWER=y
 CONFIG_FEATURE_FIND_INUM=y
+CONFIG_FEATURE_FIND_SAMEFILE=y
 CONFIG_FEATURE_FIND_EXEC=y
 CONFIG_FEATURE_FIND_EXEC_PLUS=y
 CONFIG_FEATURE_FIND_USER=y
@@ -816,10 +827,12 @@ CONFIG_FEATURE_LESS_MAXLINES=0
 # CONFIG_RFKILL is not set
 # CONFIG_RUNLEVEL is not set
 # CONFIG_RX is not set
+# CONFIG_SEEDRNG is not set
 # CONFIG_SETFATTR is not set
 # CONFIG_SETSERIAL is not set
 # CONFIG_STRINGS is not set
 CONFIG_TIME=y
+# CONFIG_TREE is not set
 # CONFIG_TS is not set
 # CONFIG_TTYSIZE is not set
 # CONFIG_UBIATTACH is not set
@@ -840,6 +853,9 @@ CONFIG_FEATURE_IPV6=y
 # CONFIG_FEATURE_UNIX_LOCAL is not set
 # CONFIG_FEATURE_PREFER_IPV4_ADDRESS is not set
 # CONFIG_VERBOSE_RESOLUTION_ERRORS is not set
+# CONFIG_FEATURE_ETC_NETWORKS is not set
+# CONFIG_FEATURE_ETC_SERVICES is not set
+# CONFIG_FEATURE_HWIB is not set
 # CONFIG_FEATURE_TLS_SHA1 is not set
 # CONFIG_ARP is not set
 CONFIG_ARPING=y
@@ -858,6 +874,7 @@ CONFIG_ARPING=y
 # CONFIG_HOSTNAME is not set
 # CONFIG_DNSDOMAINNAME is not set
 # CONFIG_HTTPD is not set
+CONFIG_FEATURE_HTTPD_PORT_DEFAULT=0
 # CONFIG_FEATURE_HTTPD_RANGES is not set
 # CONFIG_FEATURE_HTTPD_SETUID is not set
 # CONFIG_FEATURE_HTTPD_BASIC_AUTH is not set
@@ -950,6 +967,7 @@ CONFIG_FEATURE_FANCY_PING=y
 # CONFIG_FEATURE_TELNET_WIDTH is not set
 # CONFIG_TELNETD is not set
 # CONFIG_FEATURE_TELNETD_STANDALONE is not set
+CONFIG_FEATURE_TELNETD_PORT_DEFAULT=0
 # CONFIG_FEATURE_TELNETD_INETD_WAIT is not set
 # CONFIG_TFTP is not set
 # CONFIG_FEATURE_TFTP_PROGRESS_BAR is not set
@@ -987,6 +1005,7 @@ CONFIG_UDHCPC=y
 CONFIG_FEATURE_UDHCPC_ARPING=y
 CONFIG_FEATURE_UDHCPC_SANITIZEOPT=y
 CONFIG_UDHCPC_DEFAULT_SCRIPT="/usr/share/udhcpc/default.script"
+CONFIG_UDHCPC6_DEFAULT_SCRIPT=""
 # CONFIG_UDHCPC6 is not set
 # CONFIG_FEATURE_UDHCPC6_RFC3646 is not set
 # CONFIG_FEATURE_UDHCPC6_RFC4704 is not set
@@ -1014,17 +1033,19 @@ CONFIG_IFUPDOWN_UDHCPC_CMD_OPTIONS=""
 #
 # Mail Utilities
 #
+CONFIG_FEATURE_MIME_CHARSET=""
 # CONFIG_MAKEMIME is not set
 # CONFIG_POPMAILDIR is not set
 # CONFIG_FEATURE_POPMAILDIR_DELIVERY is not set
 # CONFIG_REFORMIME is not set
 # CONFIG_FEATURE_REFORMIME_COMPAT is not set
 # CONFIG_SENDMAIL is not set
-CONFIG_FEATURE_MIME_CHARSET=""
 
 #
 # Process Utilities
 #
+CONFIG_FEATURE_FAST_TOP=y
+CONFIG_FEATURE_SHOW_THREADS=y
 CONFIG_FREE=y
 CONFIG_FUSER=y
 # CONFIG_IOSTAT is not set
@@ -1063,7 +1084,6 @@ CONFIG_FEATURE_TOPMEM=y
 CONFIG_UPTIME=y
 CONFIG_FEATURE_UPTIME_UTMP_SUPPORT=y
 CONFIG_WATCH=y
-CONFIG_FEATURE_SHOW_THREADS=y
 
 #
 # Runit Utilities
@@ -1125,6 +1145,7 @@ CONFIG_ASH_MAIL=y
 CONFIG_ASH_ECHO=y
 CONFIG_ASH_PRINTF=y
 CONFIG_ASH_TEST=y
+CONFIG_ASH_SLEEP=y
 CONFIG_ASH_HELP=y
 CONFIG_ASH_GETOPTS=y
 CONFIG_ASH_CMDCMD=y

rpm/busybox-static.config
@@ -1,7 +1,7 @@
 #
 # Automatically generated make config: don't edit
-# Busybox version: 1.34.1
-# Fri Jan  7 13:50:43 2022
+# Busybox version: 1.36.1
+# Wed May 31 10:01:57 2023
 #
 CONFIG_HAVE_DOT_CONFIG=y
 
@@ -93,10 +93,16 @@ CONFIG_FEATURE_BUFFERS_USE_MALLOC=y
 # CONFIG_FEATURE_BUFFERS_GO_IN_BSS is not set
 CONFIG_PASSWORD_MINLEN=6
 CONFIG_MD5_SMALL=1
+CONFIG_SHA1_SMALL=3
+CONFIG_SHA1_HWACCEL=y
+CONFIG_SHA256_HWACCEL=y
 CONFIG_SHA3_SMALL=1
-CONFIG_FEATURE_FAST_TOP=y
-# CONFIG_FEATURE_ETC_NETWORKS is not set
-# CONFIG_FEATURE_ETC_SERVICES is not set
+CONFIG_FEATURE_NON_POSIX_CP=y
+# CONFIG_FEATURE_VERBOSE_CP_MESSAGE is not set
+CONFIG_FEATURE_USE_SENDFILE=y
+CONFIG_FEATURE_COPYBUF_KB=4
+# CONFIG_MONOTONIC_SYSCALL is not set
+CONFIG_IOCTL_HEX2STR_ERROR=y
 CONFIG_FEATURE_EDITING=y
 CONFIG_FEATURE_EDITING_MAX_LEN=1024
 # CONFIG_FEATURE_EDITING_VI is not set
@@ -120,14 +126,9 @@ CONFIG_UNICODE_WIDE_WCHARS=y
 # CONFIG_UNICODE_BIDI_SUPPORT is not set
 # CONFIG_UNICODE_NEUTRAL_TABLE is not set
 # CONFIG_UNICODE_PRESERVE_BROKEN is not set
-CONFIG_FEATURE_NON_POSIX_CP=y
-# CONFIG_FEATURE_VERBOSE_CP_MESSAGE is not set
-CONFIG_FEATURE_USE_SENDFILE=y
-CONFIG_FEATURE_COPYBUF_KB=4
-CONFIG_FEATURE_SKIP_ROOTFS=y
-# CONFIG_MONOTONIC_SYSCALL is not set
-CONFIG_IOCTL_HEX2STR_ERROR=y
-CONFIG_FEATURE_HWIB=y
+# CONFIG_LOOP_CONFIGURE is not set
+# CONFIG_NO_LOOP_CONFIGURE is not set
+CONFIG_TRY_LOOP_CONFIGURE=y
 CONFIG_XATTR=y
 
 #
@@ -163,6 +164,8 @@ CONFIG_FEATURE_BZIP2_DECOMPRESS=y
 CONFIG_CPIO=y
 CONFIG_FEATURE_CPIO_O=y
 CONFIG_FEATURE_CPIO_P=y
+CONFIG_FEATURE_CPIO_IGNORE_DEVNO=y
+CONFIG_FEATURE_CPIO_RENUMBER_INODES=y
 # CONFIG_DPKG is not set
 # CONFIG_DPKG_DEB is not set
 CONFIG_GZIP=y
@@ -198,6 +201,22 @@ CONFIG_FEATURE_LZMA_FAST=y
 #
 # Coreutils
 #
+CONFIG_FEATURE_VERBOSE=y
+
+#
+# Common options for date and touch
+#
+CONFIG_FEATURE_TIMEZONE=y
+
+#
+# Common options for cp and mv
+#
+CONFIG_FEATURE_PRESERVE_HARDLINKS=y
+
+#
+# Common options for df, du, ls
+#
+CONFIG_FEATURE_HUMAN_READABLE=y
 CONFIG_BASENAME=y
 CONFIG_CAT=y
 CONFIG_FEATURE_CATN=y
@@ -226,6 +245,7 @@ CONFIG_FEATURE_DD_IBS_OBS=y
 CONFIG_FEATURE_DD_STATUS=y
 CONFIG_DF=y
 CONFIG_FEATURE_DF_FANCY=y
+CONFIG_FEATURE_SKIP_ROOTFS=y
 CONFIG_DIRNAME=y
 CONFIG_DOS2UNIX=y
 CONFIG_UNIX2DOS=y
@@ -325,6 +345,7 @@ CONFIG_FEATURE_TR_CLASSES=y
 CONFIG_FEATURE_TR_EQUIV=y
 CONFIG_TRUE=y
 CONFIG_TRUNCATE=y
+# CONFIG_TSORT is not set
 CONFIG_TTY=y
 CONFIG_UNAME=y
 CONFIG_UNAME_OSNAME="GNU/Linux"
@@ -343,21 +364,6 @@ CONFIG_W=y
 CONFIG_USERS=y
 CONFIG_WHOAMI=y
 CONFIG_YES=y
-
-#
-# Common options
-#
-CONFIG_FEATURE_VERBOSE=y
-
-#
-# Common options for cp and mv
-#
-CONFIG_FEATURE_PRESERVE_HARDLINKS=y
-
-#
-# Common options for df, du, ls
-#
-CONFIG_FEATURE_HUMAN_READABLE=y
 
 #
 # Console Utilities
@@ -449,7 +455,11 @@ CONFIG_FEATURE_ALLOW_EXEC=y
 CONFIG_FIND=y
 CONFIG_FEATURE_FIND_PRINT0=y
 CONFIG_FEATURE_FIND_MTIME=y
+CONFIG_FEATURE_FIND_ATIME=y
+CONFIG_FEATURE_FIND_CTIME=y
 CONFIG_FEATURE_FIND_MMIN=y
+CONFIG_FEATURE_FIND_AMIN=y
+CONFIG_FEATURE_FIND_CMIN=y
 CONFIG_FEATURE_FIND_PERM=y
 CONFIG_FEATURE_FIND_TYPE=y
 CONFIG_FEATURE_FIND_EXECUTABLE=y
@@ -457,6 +467,7 @@ CONFIG_FEATURE_FIND_XDEV=y
 CONFIG_FEATURE_FIND_MAXDEPTH=y
 CONFIG_FEATURE_FIND_NEWER=y
 CONFIG_FEATURE_FIND_INUM=y
+CONFIG_FEATURE_FIND_SAMEFILE=y
 CONFIG_FEATURE_FIND_EXEC=y
 CONFIG_FEATURE_FIND_EXEC_PLUS=y
 CONFIG_FEATURE_FIND_USER=y
@@ -828,10 +839,12 @@ CONFIG_READAHEAD=y
 # CONFIG_RFKILL is not set
 CONFIG_RUNLEVEL=y
 CONFIG_RX=y
+# CONFIG_SEEDRNG is not set
 CONFIG_SETFATTR=y
 CONFIG_SETSERIAL=y
 CONFIG_STRINGS=y
 CONFIG_TIME=y
+# CONFIG_TREE is not set
 # CONFIG_TS is not set
 CONFIG_TTYSIZE=y
 CONFIG_UBIATTACH=y
@@ -852,6 +865,9 @@ CONFIG_FEATURE_IPV6=y
 # CONFIG_FEATURE_UNIX_LOCAL is not set
 CONFIG_FEATURE_PREFER_IPV4_ADDRESS=y
 # CONFIG_VERBOSE_RESOLUTION_ERRORS is not set
+# CONFIG_FEATURE_ETC_NETWORKS is not set
+# CONFIG_FEATURE_ETC_SERVICES is not set
+CONFIG_FEATURE_HWIB=y
 # CONFIG_FEATURE_TLS_SHA1 is not set
 CONFIG_ARP=y
 CONFIG_ARPING=y
@@ -870,6 +886,7 @@ CONFIG_FEATURE_FTPGETPUT_LONG_OPTIONS=y
 CONFIG_HOSTNAME=y
 CONFIG_DNSDOMAINNAME=y
 CONFIG_HTTPD=y
+CONFIG_FEATURE_HTTPD_PORT_DEFAULT=0
 CONFIG_FEATURE_HTTPD_RANGES=y
 CONFIG_FEATURE_HTTPD_SETUID=y
 CONFIG_FEATURE_HTTPD_BASIC_AUTH=y
@@ -962,6 +979,7 @@ CONFIG_FEATURE_TELNET_AUTOLOGIN=y
 CONFIG_FEATURE_TELNET_WIDTH=y
 CONFIG_TELNETD=y
 CONFIG_FEATURE_TELNETD_STANDALONE=y
+CONFIG_FEATURE_TELNETD_PORT_DEFAULT=0
 CONFIG_FEATURE_TELNETD_INETD_WAIT=y
 CONFIG_TFTP=y
 CONFIG_FEATURE_TFTP_PROGRESS_BAR=y
@@ -999,6 +1017,7 @@ CONFIG_UDHCPC=y
 CONFIG_FEATURE_UDHCPC_ARPING=y
 CONFIG_FEATURE_UDHCPC_SANITIZEOPT=y
 CONFIG_UDHCPC_DEFAULT_SCRIPT="/usr/share/udhcpc/default.script"
+CONFIG_UDHCPC6_DEFAULT_SCRIPT=""
 # CONFIG_UDHCPC6 is not set
 # CONFIG_FEATURE_UDHCPC6_RFC3646 is not set
 # CONFIG_FEATURE_UDHCPC6_RFC4704 is not set
@@ -1026,17 +1045,19 @@ CONFIG_LPQ=y
 #
 # Mail Utilities
 #
+CONFIG_FEATURE_MIME_CHARSET="us-ascii"
 CONFIG_MAKEMIME=y
 CONFIG_POPMAILDIR=y
 CONFIG_FEATURE_POPMAILDIR_DELIVERY=y
 CONFIG_REFORMIME=y
 CONFIG_FEATURE_REFORMIME_COMPAT=y
 CONFIG_SENDMAIL=y
-CONFIG_FEATURE_MIME_CHARSET="us-ascii"
 
 #
 # Process Utilities
 #
+CONFIG_FEATURE_FAST_TOP=y
+CONFIG_FEATURE_SHOW_THREADS=y
 CONFIG_FREE=y
 CONFIG_FUSER=y
 CONFIG_IOSTAT=y
@@ -1075,7 +1096,6 @@ CONFIG_FEATURE_TOPMEM=y
 CONFIG_UPTIME=y
 CONFIG_FEATURE_UPTIME_UTMP_SUPPORT=y
 CONFIG_WATCH=y
-CONFIG_FEATURE_SHOW_THREADS=y
 
 #
 # Runit Utilities
@@ -1137,6 +1157,7 @@ CONFIG_ASH_EXPAND_PRMT=y
 CONFIG_ASH_ECHO=y
 CONFIG_ASH_PRINTF=y
 CONFIG_ASH_TEST=y
+CONFIG_ASH_SLEEP=y
 CONFIG_ASH_HELP=y
 CONFIG_ASH_GETOPTS=y
 CONFIG_ASH_CMDCMD=y

rpm/busybox.spec
@@ -1,6 +1,6 @@
 Summary: Single binary providing simplified versions of system commands
 Name: busybox
-Version: 1.34.1
+Version: 1.36.1
 Release: 1
 License: GPLv2
 Source0: http://www.busybox.net/downloads/%{name}-%{version}.tar.bz2

rpm/0001-Copy-extended-attributes-if-p-flag-is-provided-to-cp.patch
@@ -29,10 +29,10 @@ Signed-off-by: Igor Zhbanov <igor.zhbanov@jolla.com>
  create mode 100644 libbb/copy_file_attr.c
 
 diff --git a/include/libbb.h b/include/libbb.h
-index cae54658b..d3f67b5fb 100644
+index cca33a177faba9669eb1c2499c50663ca883385c..c03712e1f6071071a04710c6fb8ab78ede19d90e 100644
 --- a/include/libbb.h
 +++ b/include/libbb.h
-@@ -457,6 +457,9 @@ extern int remove_file(const char *path, int flags) FAST_FUNC;
+@@ -506,6 +506,9 @@ extern int remove_file(const char *path, int flags) FAST_FUNC;
   * This makes "cp /dev/null file" and "install /dev/null file" (!!!)
   * work coreutils-compatibly. */
  extern int copy_file(const char *source, const char *dest, int flags) FAST_FUNC;
@@ -43,13 +43,13 @@ index cae54658b..d3f67b5fb 100644
  enum {
  	ACTION_RECURSE        = (1 << 0),
 diff --git a/libbb/Config.src b/libbb/Config.src
-index f97de8ef7..520966791 100644
+index b980f19a92738dc6a7a1174e1ddb2d271c32d04a..b4bddbe5840cd8ea2ad254393f517209da0bd9a4 100644
 --- a/libbb/Config.src
 +++ b/libbb/Config.src
-@@ -395,3 +395,9 @@ config FEATURE_HWIB
- 	default y
- 	help
- 	Support for printing infiniband addresses in network applets.
+@@ -391,3 +391,9 @@ config TRY_LOOP_CONFIGURE
+ 	bool "try LOOP_CONFIGURE, fall back to LOOP_SET_FD + LOOP_SET_STATUS"
+ 
+ endchoice
 +
 +config XATTR
 +	bool "Support preserve extended attributes for cp command"
@@ -57,20 +57,20 @@ index f97de8ef7..520966791 100644
 +	help
 +	  Support preserve extended attributes for cp and mv command.
 diff --git a/libbb/Kbuild.src b/libbb/Kbuild.src
-index 676300801..bb5ecb9ef 100644
+index 653025e56c76c3b8e44f4b15ac4d6b7d0cf8d70f..220240b99679f7cba4990242b2233a4d845139c3 100644
 --- a/libbb/Kbuild.src
 +++ b/libbb/Kbuild.src
-@@ -199,3 +199,5 @@ lib-$(CONFIG_FEATURE_FIND_REGEX) += xregcomp.o
+@@ -206,3 +206,5 @@ lib-$(CONFIG_FEATURE_CUT_REGEX) += xregcomp.o
  
  # Add the experimental logging functionality, only used by zcip
  lib-$(CONFIG_ZCIP) += logenv.o
 +
 +lib-$(CONFIG_XATTR) += copy_file_attr.o
 diff --git a/libbb/copy_file.c b/libbb/copy_file.c
-index 49d1ec9c6..0f64a1e31 100644
+index 044bc3c20843fa21dc6c8af468326e6a39ce595b..1327fc9df0725d49389eac591bd6ba7e2007274a 100644
 --- a/libbb/copy_file.c
 +++ b/libbb/copy_file.c
-@@ -434,6 +434,13 @@ int FAST_FUNC copy_file(const char *source, const char *dest, int flags)
+@@ -436,6 +436,13 @@ int FAST_FUNC copy_file(const char *source, const char *dest, int flags)
  			source_stat.st_mode &= ~(S_ISUID | S_ISGID);
  			bb_perror_msg("can't preserve %s of '%s'", "ownership", dest);
  		}
@@ -86,7 +86,7 @@ index 49d1ec9c6..0f64a1e31 100644
  	}
 diff --git a/libbb/copy_file_attr.c b/libbb/copy_file_attr.c
 new file mode 100644
-index 000000000..180b0311e
+index 0000000000000000000000000000000000000000..180b0311e6e3665204d02b29373a77f32321177c
 --- /dev/null
 +++ b/libbb/copy_file_attr.c
 @@ -0,0 +1,125 @@
@@ -215,6 +215,3 @@ index 000000000..180b0311e
 +	bb_free(names);
 +	return ret;
 +}
--- 
-2.31.1
-

rpm/0002-applets-Busybox-in-usr-bin-instead-of-bin.patch
@@ -8,7 +8,7 @@ Subject: [PATCH] applets: Busybox in /usr/bin instead of /bin
  1 file changed, 12 insertions(+), 12 deletions(-)
 
 diff --git a/applets/install.sh b/applets/install.sh
-index 415896893..04f0526c8 100755
+index 415896893e8626ec26f6790bdffdf5adc7581331..04f0526c88ecbbd135a39a29487b254cb521b8a1 100755
 --- a/applets/install.sh
 +++ b/applets/install.sh
 @@ -60,8 +60,8 @@ if [ -n "$DO_INSTALL_LIBS" ] && [ x"$DO_INSTALL_LIBS" != x"n" ]; then
@@ -71,6 +71,3 @@ index 415896893..04f0526c8 100755
  			;;
  			*)
  				echo "Unknown installation directory: $appdir"
--- 
-2.31.1
-

commit 05492272462b40443ca5861b2bf60ecfe31306df

busybox: Disable sleep as ash builtin which breaks recovery
Busybox's ash has the option for sleep to a builtin. Builtin
in this context meaning as a command rather than as external program
that doesn't require a process to be created for each call to a
builtin command.

Sleep being run inside the shell process it self can cause various
issue, which is the reason upstream disabled the option.

In our case the bug builtin sleep triggered was caused by a later
commit which was supposed to fix a bug in builtin sleep but only
caused for the command to be interrupted by SIGCHILD.
For sleep to not well sleep caused in turn then for the shell to exit
before intended.

Because sleep didn't wait the recovery init exited which
caused the kernel to panic as init is not supposed to exit by itself.

Further sleep as builtin also causes ash to exit when the user passes
wrong arguments such as 1ab or 0.5.

[busybox] Disable sleep as ash builtin which breaks recovery. Fixes JB#61382

See-also: https://git.busybox.net/busybox/commit/?id=5e0411a7fb510b9aecda0a850c76bdd62c50efa4
See-also: https://bugs.busybox.net/show_bug.cgi?id=15619
Fixes: be6020a4aca08f1e30d73f7cf1ac20342adca1f4
Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>

rpm/0004-ash-Load-ENV-file-also-if-SSH_CLIENT-SSH2_CLIENT-is-.patch
@@ -15,7 +15,7 @@ Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>
  1 file changed, 1 insertion(+)
 
 diff --git a/shell/ash.c b/shell/ash.c
-index 9344e4de1729c464af04d70754b2511af7af0cbe..cb2d4d4c3112ba5e8c4f846a217a21fce49d8b7c 100644
+index 9344e4de1729c464af04d70754b2511af7af0cbe..049089677f8098cde55da7ef76dce72fe3408495 100644
 --- a/shell/ash.c
 +++ b/shell/ash.c
 @@ -14726,6 +14726,7 @@ int ash_main(int argc UNUSED_PARAM, char **argv)

rpm/0005-ash-job-option-to-restore-term-io-after-job-is-stopp.patch
@@ -17,7 +17,7 @@ Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>
  1 file changed, 9 insertions(+), 1 deletion(-)
 
 diff --git a/shell/ash.c b/shell/ash.c
-index cb2d4d4c3112ba5e8c4f846a217a21fce49d8b7c..9f07632f6ce16b8b3d144470f1c37bc4b0035779 100644
+index 049089677f8098cde55da7ef76dce72fe3408495..fad94fdb2b2a436f29fc56e87fbe7d5bf6fbac1d 100644
 --- a/shell/ash.c
 +++ b/shell/ash.c
 @@ -77,6 +77,14 @@

rpm/0006-ash-Write-history-on-SIGHUP.patch
@@ -16,7 +16,7 @@ Signed-off-by: Tomi Leppänen <tomi.leppanen@jolla.com>
  1 file changed, 20 insertions(+)
 
 diff --git a/shell/ash.c b/shell/ash.c
-index 9f07632f6ce16b8b3d144470f1c37bc4b0035779..79c30d4c9dad76a7963d89742e6a496891b398cf 100644
+index fad94fdb2b2a436f29fc56e87fbe7d5bf6fbac1d..d81c1354a370c138548d9482afd38baff4a189e6 100644
 --- a/shell/ash.c
 +++ b/shell/ash.c
 @@ -3692,6 +3692,19 @@ signal_handler(int signo)

rpm/0008-ash-disable-sleep-as-builtin-closes-15619.patch
@@ -1,0 +1,72 @@
+From 0000000000000000000000000000000000000000 Mon Sep 17 00:00:00 2001
+From: Denys Vlasenko <vda.linux@googlemail.com>
+Date: Mon, 10 Jul 2023 10:52:41 +0200
+Subject: [PATCH] ash: disable sleep as builtin, closes 15619
+MIME-Version: 1.0
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
+
+Has a few annoying problems:
+* sleepcmd() -> sleep_main(), the parsing of bad arguments exits the shell.
+* sleep_for_duration() in sleep_main() has to be interruptible for
+  ^C traps to work, which may be a problem for other users
+  of sleep_for_duration().
+* BUT, if sleep_for_duration() is interruptible, then SIGCHLD interrupts it
+  as well (try "/bin/sleep 1 & sleep 10").
+* sleep_main() must not allocate anything as ^C in ash longjmp's.
+  (currently, allocations are only on error paths, in message printing).
+
+Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>
+(cherry picked from commit 5e0411a7fb510b9aecda0a850c76bdd62c50efa4)
+Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>
+---
+ include/libbb.h |  2 ++
+ shell/ash.c     | 22 +++++++++++++++++-----
+ 2 files changed, 19 insertions(+), 5 deletions(-)
+
+diff --git a/include/libbb.h b/include/libbb.h
+index c03712e1f6071071a04710c6fb8ab78ede19d90e..f22f19d45eb6ac206cbcb7c63b24472ca636e991 100644
+--- a/include/libbb.h
++++ b/include/libbb.h
+@@ -1514,6 +1514,8 @@ int hush_main(int argc, char** argv) IF_SHELL_HUSH(MAIN_EXTERNALLY_VISIBLE);
+ /* If shell needs them, they exist even if not enabled as applets */
+ int echo_main(int argc, char** argv) IF_ECHO(MAIN_EXTERNALLY_VISIBLE);
+ int sleep_main(int argc, char **argv) IF_SLEEP(MAIN_EXTERNALLY_VISIBLE);
++/* See disabled "config ASH_SLEEP" in ash.c */
++#define ENABLE_ASH_SLEEP 0
+ int printf_main(int argc, char **argv) IF_PRINTF(MAIN_EXTERNALLY_VISIBLE);
+ int test_main(int argc, char **argv)
+ #if ENABLE_TEST || ENABLE_TEST1 || ENABLE_TEST2
+diff --git a/shell/ash.c b/shell/ash.c
+index 558bd58848d13fa8749b4a6ad3c1014d5994dae4..643eef9b0e07fdece43f8f5f7afbcf4eac2b1cfb 100644
+--- a/shell/ash.c
++++ b/shell/ash.c
+@@ -142,11 +142,23 @@
+ //config:	default y
+ //config:	depends on SHELL_ASH
+ //config:
+-//config:config ASH_SLEEP
+-//config:	bool "sleep builtin"
+-//config:	default y
+-//config:	depends on SHELL_ASH
+-//config:
++//
++////config:config ASH_SLEEP
++////config:	bool "sleep builtin"
++////config:	default y
++////config:	depends on SHELL_ASH
++////config:
++//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
++//Disabled for now. Has a few annoying problems:
++// * sleepcmd() -> sleep_main(), the parsing of bad arguments exits the shell.
++// * sleep_for_duration() in sleep_main() has to be interruptible for
++//   ^C traps to work, which may be a problem for other users
++//   of sleep_for_duration().
++// * BUT, if sleep_for_duration() is interruptible, then SIGCHLD interrupts it
++//   as well (try "/bin/sleep 1 & sleep 10").
++// * sleep_main() must not allocate anything as ^C in ash longjmp's.
++//   (currently, allocations are only on error paths, in message printing).
++//
+ //config:config ASH_HELP
+ //config:	bool "help builtin"
+ //config:	default y

rpm/busybox-sailfish.config
@@ -1145,7 +1145,6 @@ CONFIG_ASH_MAIL=y
 CONFIG_ASH_ECHO=y
 CONFIG_ASH_PRINTF=y
 CONFIG_ASH_TEST=y
-CONFIG_ASH_SLEEP=y
 CONFIG_ASH_HELP=y
 CONFIG_ASH_GETOPTS=y
 CONFIG_ASH_CMDCMD=y

rpm/busybox-static.config
@@ -1157,7 +1157,6 @@ CONFIG_ASH_EXPAND_PRMT=y
 CONFIG_ASH_ECHO=y
 CONFIG_ASH_PRINTF=y
 CONFIG_ASH_TEST=y
-CONFIG_ASH_SLEEP=y
 CONFIG_ASH_HELP=y
 CONFIG_ASH_GETOPTS=y
 CONFIG_ASH_CMDCMD=y

rpm/busybox.spec
@@ -15,6 +15,8 @@ Patch3:  0004-ash-Load-ENV-file-also-if-SSH_CLIENT-SSH2_CLIENT-is-.patch
 Patch4:  0005-ash-job-option-to-restore-term-io-after-job-is-stopp.patch
 Patch5:  0006-ash-Write-history-on-SIGHUP.patch
 Patch6:  0007-shell-fix-SIGWINCH-and-SIGCHLD-in-hush-interrupting-.patch
+Patch7:  0008-ash-disable-sleep-as-builtin-closes-15619.patch
+
 URL: https://github.com/sailfishos/busybox
 BuildRequires: glibc-static
 BuildRequires: libselinux-static libsepol-static